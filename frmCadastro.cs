6b049a871782ded59cb57a42d1b9ba908929a210
Gabriel Gutierri <gabrielgutierri04@gmail.com>
2022-05-07 13:49:45 -0300

    removing connection string

--- frmCadastro.cs
+++ frmCadastro.cs
@@ -25,7 +25,7 @@ namespace Software_Trelisa
         {
             try
             {
-                string connectionString = $"Data Source=truss-server.database.windows.net;Initial Catalog=TrussDatabase;User ID=TrussAdmin;Password=TrussGVMJF0422";
+                string connectionString = $"Data Source=truss-server.database.windows.net;Initial Catalog=TrussDatabase;User ID=;Password=";
                 SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
 
                 builder.DataSource = "truss-server.database.windows.net";
